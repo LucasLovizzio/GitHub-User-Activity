8ce24737b0ecabed943e493ce3ff29e1943bead6
Lucas <llovizzio@gmail.com>
2025-01-20 17:26:04 -0300

    funtional script

--- main.py
+++ main.py
@@ -1,18 +1,36 @@
+import argparse
 import requests
+import sys
 
-username = input("Enter your github username: ")
+def fetch_github_activity(ussername : string):
+    url = f'https://api.github.com/users/{username}/events'
+    try :
+        response = requests.get(url)
+        response.raise_for_status()
+        events = response.json()
 
-url = f"https://api.github.com/users/{username}/events/public"
+        if not events:
+            print(f'No recent activity found for user : {username}')
+            return
+        
+        print(f"Recent activity for Github user '{username}': \n")
+        for event in events[:10]
+            event_type = event['type']
+            repo_name = event['repo']['name']
+            print(f"- {event_type} in {repo_name}")
+            
+    except requests.exceptions.RequestException as e:
+        print(f"Error fetching activity: {e}", file = sys.stderr)
+    except KeyError:
+        print("Unexpected response format from GitHub API.", file=sys.stderr)
 
-response = requests.get(url)
+def main():
+    """Main entry point for the CLI."""
+    parser = argparse.ArgumentParser(description="Fetch recent GitHub activity for a user.")
+    parser.add_argument("username", help="GitHub username to fetch activity for")
+    args = parser.parse_args()
 
-if response.status_code == 200:
-    events = response.json()
-    for event in events:
-        print(event)
-else:
-    print("Error fetching events")
+    fetch_github_activity(args.username)
 
-# Run the script
-# python main.py
-# Enter your github username: your_github_username
+if __name__ == "__main__":
+    main()
